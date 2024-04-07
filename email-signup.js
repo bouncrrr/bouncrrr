927313ea1b8df9ebbe7f594e1352e7e5a444f91e
bouncrrr <165423752+bouncrrr@users.noreply.github.com>
2024-04-07 15:31:45 +0100

    Update email-signup.js

--- email-signup.js
+++ email-signup.js
@@ -4,23 +4,23 @@ document.getElementById('signup-form').addEventListener('submit', function(e) {
     const email = document.getElementById('email').value;
     const consent = true; // Assuming the user implicitly gives consent by submitting the form, adjust as needed
 
-    fetch('https://paddle-cockroach-bouncrrr.onrender.com/api/email-signup', {
+    fetch('https://bouncrrr-paddle-backend.onrender.com/api/email-signup', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({ email, consent }),
     })
-    .then(response => {
+    .then(response => response.json()) // Assuming your backend sends a JSON response
     if (response.ok) {
         alert('thank you for signing up!');
     } else if (response.status === 409) { // Check for the 409 Conflict status
-        alert('this email is already signed up');
+        alert('this email is already signed up.');
     } else {
-        alert('there was an issue with your signup. please try again');
+        alert('there was an issue with your signup. please try again.');
     }
 })
 .catch((error) => {
-    console.error('error:', error);
+    console.error('Error:', error);
     alert('error submitting your email');
 });
